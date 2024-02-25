8e483fe2622cf48e44147f7f469b5b621e77d242
Sarah Allen <sarah@ultrasaurus.com>
2024-02-25 14:24:35 -0800

    hello world from file, render HTML

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ use axum::{
     Router,
 };
 use tracing::{info};
+mod web;
+use web::render;
+// use std::borrow::Cow;
 
 #[tokio::main]
 async fn main() {
     // install global subscriber configured based on RUST_LOG envvar.
     tracing_subscriber::fmt::init();
     info!("Logging enabled");
-
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(render()));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -21,8 +23,8 @@ async fn main() {
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    info!("root");
-    "Hello, World!"
-}
+// async fn root() -> String {
+//     info!("root");
+//     render().into()
+// }
 
---
+++ src/web.rs
@@ -0,0 +1,27 @@
+use anyhow::{Result, Error, bail};
+use std::fs::File;
+use std::borrow::Cow;
+use std::io::Read;
+use tracing::info;
+use axum::response::Html;
+
+fn read_file(filename: &str) -> Result<String> {
+    let mut f = File::open(filename)?;
+    let mut buf = String::new();
+    let bytes = f.read_to_string(&mut buf)?;
+    if bytes == 0 {
+      bail!("failed to read: 0 bytes returned from read_to_string");  
+    } 
+    Ok(buf)
+}
+
+pub fn render() -> Html<Cow<'static, str>> {     
+    let result = read_file("source/index.html");
+    match result {
+        Ok(s) => Html(s.into()),
+        Err(e) => {
+            let error = format!("Error #{:?}", e);
+            Html(error.into())
+        }
+    }
+}
