d918f3e22af601776664cbe5ce3f5d105d28ac98
Sarah Allen <sarah@ultrasaurus.com>
2024-03-15 23:49:43 -0700

    remove unused code

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,2 @@
 mod dir_entry;
 pub use self::dir_entry::DirEntryExt;
-
-// pub mod path;
-// pub use self::path::PathExt;
--- src/util/path.rs
+++
@@ -1,8 +0,0 @@
-// use std::fmt;
-// use std::path::Path;
-
-// impl fmt::Display for Path {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "{}", self.display())
-//     }
-// }
