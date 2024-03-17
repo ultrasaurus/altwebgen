6277e971bc413d0af8696bf64ca3129b2a0a0a88
Sarah Allen <sarah@ultrasaurus.com>
2024-03-17 16:38:43 -0700

    fix comments

--- src/devserve/mod.rs
+++ src/devserve/mod.rs
@@ -1,14 +1,13 @@
-use tracing::{error, info};
+use tracing::info;
 use warp::Filter;
 use crate::config::Config;
-
-
-// use std::borrow::Cow;
 
 pub async fn run(config: &Config) -> anyhow::Result<()> {
     info!("devserve::run");
     let website_dir = config.outdir.canonicalize()?;
     info!("serve website_dir: {}", website_dir.display());
+
+    // GET / => index.html
     let root = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file(website_dir.join("index.html")));
@@ -16,8 +15,6 @@ pub async fn run(config: &Config) -> anyhow::Result<()> {
     // dir already requires GET...
     let all = warp::fs::dir(website_dir);
 
-    // GET / => index.html
-    // GET /... => ./examples/..
     let routes = root.or(all)
                 .with(warp::trace::request());
 
