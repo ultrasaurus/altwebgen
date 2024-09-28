51f485b7b44a6febb44c2423436a088dc35d8a9b
Sarah Allen <sarah@ultrasaurus.com>
2024-09-27 19:46:29 -0700

    support input file, add apostrophe to example

--- examples/words.rs
+++ examples/words.rs
@@ -1,7 +1,7 @@
 use words::html_words;
 
 fn main() -> anyhow::Result<()> {
-    let text = "2 much is never enough 4me... let's see how punctuation works?";
+    let text = "2 much is never enough 4me... does punctuation work?";
     let html_string = html_words(text)?;
 
     println!("-----");
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,31 @@ struct Cli {
     /// source text 
     #[clap(short, long, value_parser, default_value = "Hello world!")]
     text: String,
+    /// input file, if --input option is given, then --text is ignored
+    #[clap(short, long, value_parser)]
+    input: Option<String>,
+    /// output file, if not given output to stdio
+    #[clap(short, long, value_parser)]
+    output: Option<String>,
+
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    let html_string = html_words(&cli.text)?;
 
-    println!("-----");
-    println!("{}", html_string);
+    let html_string = match cli.input {
+        None => html_words(&cli.text)?,
+        Some(path) => {
+            let input = std::fs::read_to_string(path)?;
+            html_words(input)?
+        }
+    };
 
-    println!("-----");
+    match cli.output {
+        None =>   println!("{}", html_string),
+        Some(path) => std::fs::write(path, html_string)?
+    }
+  
 
     Ok(())
 }
