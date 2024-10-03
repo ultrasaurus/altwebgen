045d33e24822bb66133050fc47c1b9181a561f75
Sarah Allen <sarah@ultrasaurus.com>
2024-10-03 16:16:47 -0700

    starter action for deploy of samples
    rename binary in preparation for eventually merging with webgenr
    copied my old code from webgenr for adding date and sha to index page
    build just one sample for now + index page that does not yet link to it

---
+++ gh-pages/build.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+(
+cd gh-pages/files
+
+INDEX_FILE="source/markdown/index.md"
+echo $INDEX_FILE
+
+DATE_STRING=$(date +"%a, %b %d %Y - %I:%M %p")
+echo $DATE_STRING
+
+cat <<EOT >> $INDEX_FILE
+\`\`\`
+$DATE_STRING
+branch=$BRANCH_NAME
+$GITHUB_SHA
+\`\`\`
+EOT
+
+
+webgenr
+)
+
+
---
+++ gh-pages/files/markdown/index.md
@@ -0,0 +1,1 @@
+# Demos will go here
