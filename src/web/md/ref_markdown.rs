a147ce762d0d3561f1354a78abbaaaece7d24735
Sarah Allen <sarah@ultrasaurus.com>
2024-10-08 13:27:07 -0700

    make fn private that is not called externally

--- src/web/md/ref_markdown.rs
+++ src/web/md/ref_markdown.rs
@@ -25,7 +25,7 @@ impl Ref {
     pub fn new() -> Self {
         Ref { md: None, audio: None, transcript: None }
     }
-    pub fn write_html<W: Write>(&self, mut writer: W) -> anyhow::Result<()> {
+    fn write_html<W: Write>(&self, mut writer: W) -> anyhow::Result<()> {
         trace!("write_html for ref: {:?}", self);
         if let Some(audio) = &self.audio {
             trace!("write_html audio file_name: {:?}", audio.path.file_name());
