33fe1c061fa63ef41e09311e4649e0a79b313b0a
Sarah Allen <sarah@ultrasaurus.com>
2024-10-09 15:23:45 -0700

    generate_whisperx_json seems to work
    very basic test does not (yet) verify output,
    just tests that the call fails without error

--- src/web/audio/whisperx.rs
+++ src/web/audio/whisperx.rs
@@ -1,23 +1,59 @@
-use std::path::Path;
+use std::{fs, path::Path};
 use std::process::Command;
-use tracing::warn;
-use std::fs::OpenOptions;
+use tracing::{error, info, trace, warn};
 
-pub fn gen_transcript<P: AsRef<Path>>(audio: P, transcript: P) -> anyhow::Result<()> {
-    generate_whisperx_json(audio, transcript)
+pub fn gen_transcript(
+        audio: impl AsRef<Path>,
+        transcript: impl AsRef<Path>
+) -> anyhow::Result<()> {
+    let root_path = Path::new(".");
+    let outpath = transcript.as_ref().parent().unwrap_or(root_path);
+
+    generate_whisperx_json(audio, outpath)
+    // TODO: convert transcript to podcast format
 }
 
-fn generate_whisperx_json<P: AsRef<Path>>(audio: P, transcript: P) -> anyhow::Result<()> {
-    let outpath = transcript.as_ref();
-    if outpath.is_file() {
-        warn!("existing whisper transcript file will be overwritten: {}", outpath.to_string_lossy())
+fn generate_whisperx_json(
+    audio: impl AsRef<Path>,
+    output_dir: impl AsRef<Path>
+) -> anyhow::Result<()> {
+    info!("generate_whisperx_json");
+    let outpath = output_dir.as_ref();
+    let inpath = audio.as_ref();
+    fs::create_dir_all(outpath)?;
+
+let output: std::process::Output = Command::new("pwd")
+    .output()
+    .expect("Failed to execute pwd command");
+println!("pwd: {}", String::from_utf8_lossy(&output.stdout));
+println!("calling: whisperx {} --language en", inpath.to_string_lossy());
+
+    match Command::new("whisperx")
+    .arg(inpath.as_os_str())
+    .arg("--output_format")
+    .arg("json")
+    .arg("--compute_type")
+    .arg("float32")
+    .arg("--output_dir")
+    .arg(outpath.as_os_str())
+    .arg("--language")
+    .arg("en")
+    .output()
+    {
+        Ok(cmd_output) => {
+            println!("status: {:?}", cmd_output.status);
+            let stdout = String::from_utf8(cmd_output.stdout).unwrap();
+            println!("whisperx stdout:\n{}\n\n", stdout);
+
+            let stderr = String::from_utf8(cmd_output.stderr).unwrap();
+            println!("whisperx stderr:\n{}\n\n", stderr);
+            Ok(())
+        },
+        Err(e) => {
+            Err(anyhow::Error::new(e).context("failed to execute whisperx command"))
+        }
     }
-    let f = OpenOptions::new()
-        .write(true).create(true)
-        .open(outpath)?;
-
-    Ok(())
-}
+    }
 
 #[cfg(test)]
 mod tests {
@@ -25,21 +61,18 @@ mod tests {
     use std::path::Path;
 
     #[test]
-    fn test_gen_transcript() {
+    fn test_generate_whisperx_json() {
         let infile = Path::new("src/test/data/short-sentence.mp3");
-        let outfile = Path::new("src/test/data/short-sentence..whisperx.json");
-        // let outfile = infile.with_extension(".whisperx.json");
-        gen_transcript(&infile, &outfile).unwrap();
+        let outfile = Path::new("src/test/data");
+        generate_whisperx_json(&infile, &outfile).unwrap();
     }
-}
-
-
-
-#[cfg(tests)]
-mod tests {
-    use super::*;
 
     #[test]
-    fn test_whisperx() {
+    fn test_gen_transcript() {
+        let infile = Path::new("src/test/data/short-sentence.mp3");
+        let outfile = Path::new("src/test/data/short-sentence.transcript.json");
+        generate_whisperx_json(&infile, &outfile).unwrap();
     }
 }
+
+
