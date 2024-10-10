09f6eb621dd0fdb3c825d3b5f61d2ab894379019
Sarah Allen <sarah@ultrasaurus.com>
2024-10-10 13:42:18 -0700

    rust gen_transcript function calls transcript converter

--- src/web/audio/whisperx.rs
+++ src/web/audio/whisperx.rs
@@ -1,3 +1,4 @@
+use anyhow::bail;
 use std::{fs, path::Path};
 use std::process::Command;
 use tracing::{error, info, trace, warn};
@@ -6,12 +7,60 @@ pub fn gen_transcript(
         audio: impl AsRef<Path>,
         transcript: impl AsRef<Path>
 ) -> anyhow::Result<()> {
-    let root_path = Path::new(".");
-    let outpath = transcript.as_ref().parent().unwrap_or(root_path);
+    info!("gen_transcript in: {} out:{}",
+        audio.as_ref().display(), transcript.as_ref().display());
+    println!("gen_transcript in: {} out:{}",
+        audio.as_ref().display(), transcript.as_ref().display());
 
-    generate_whisperx_json(audio, outpath)
-    // TODO: convert transcript to podcast format
+    let audio_filename = match audio.as_ref().file_name() {
+        None => bail!("audio path must refer to a file"),
+        Some(name) => name
+    };
+
+    let root_path = Path::new(".");
+    let out_dir = transcript.as_ref().parent().unwrap_or(root_path);
+
+    generate_whisperx_json(&audio, out_dir)?;
+    let audio_outpath = out_dir.join(audio_filename);
+    println!("audio_outpath = {}", audio_outpath.display());
+    let whisper_json_path = audio_outpath.with_extension("json");
+    convert_to_transcript_json(&whisper_json_path, transcript )
 }
+
+fn convert_to_transcript_json(
+        whisper: impl AsRef<Path>,
+        podcast: impl AsRef<Path>
+) -> anyhow::Result<()> {
+    println!("convert_to_transcript_json in: {} out:{}",
+        whisper.as_ref().to_string_lossy(), podcast.as_ref().to_string_lossy());
+
+    let cmd_path = match std::env::home_dir() {
+        None => bail!("couldn't find $HOME directory - looking for transcript-converter path"),
+        Some(home) => home.join("transcript-converter/transcriptConverter.py")
+    };
+
+    // python transcript-converter/transcriptConverter.py
+    match Command::new("python")
+        .arg(cmd_path.as_os_str())
+        .arg(whisper.as_ref().as_os_str())
+        .arg(podcast.as_ref().as_os_str())
+        .output() {
+        Ok(cmd_output) => {
+            println!("status: {:?}", cmd_output.status);
+            let stdout = String::from_utf8(cmd_output.stdout).unwrap();
+            println!("transcriptConverter stdout:\n{}\n\n", stdout);
+
+            let stderr = String::from_utf8(cmd_output.stderr).unwrap();
+            println!("transcriptConverter stderr:\n{}\n\n", stderr);
+            Ok(())
+        },
+        Err(e) => {
+            Err(anyhow::Error::new(e).context("failed to execute transcriptConverter command"))
+        }
+
+    }
+}
+
 
 fn generate_whisperx_json(
     audio: impl AsRef<Path>,
@@ -63,15 +112,15 @@ mod tests {
     #[test]
     fn test_generate_whisperx_json() {
         let infile = Path::new("src/test/data/short-sentence.mp3");
-        let outfile = Path::new("src/test/data");
-        generate_whisperx_json(&infile, &outfile).unwrap();
+        let outdir = Path::new("src/test/data");
+        generate_whisperx_json(&infile, &outdir).unwrap();
     }
 
     #[test]
     fn test_gen_transcript() {
         let infile = Path::new("src/test/data/short-sentence.mp3");
         let outfile = Path::new("src/test/data/short-sentence.transcript.json");
-        generate_whisperx_json(&infile, &outfile).unwrap();
+        gen_transcript(&infile, &outfile).unwrap();
     }
 }
 
